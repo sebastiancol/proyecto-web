f1e149408bfadfeabef09ccc2d4089634967eb49
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2019-05-06 22:07:33 -0500

    Se edita logica para estudiantes

--- JS/LogicaAdministrador.js
+++ JS/LogicaAdministrador.js
@@ -12,168 +12,20 @@
             },            
             error: function (xhr, status) {
                 alert('Disculpe, existió un problema');
-            },
-            
+            }
         });
-    } 
+     }
     
     $("#Administrador").click(function(){        
             let url= "RDM/Administrador/menuadmin.html";
             let llevo= "acceso=true";
             let hacer = function(data){
                 $("#rdm").html(data);
-                Administrador();
             };
         Llamado(url,llevo,hacer);
     });
 
-     //Menú administrador
-     //----Crear estudiante
-     $(document).on("click", "#crear_est", function () {
-         let url = "RDM/Administrador/addestudiante.html";
-         let llevo = "acceso=true";
-         let hacer = function(data){
-             $("#rdm").html(data);
-             //Administrador();
-         };
-         Llamado(url,llevo,hacer);
-     });
-    //-----Menú adminstrador
 
-
-    function Administrador(){
-        $("#ADDDocentes").click(function(){
-             
-            var url= "RDM/Docentes/adddocentes.html";
-            var llevo= "acceso=true";
-            var hacer=function(data){
-                $("#rdm").html(data);
-                
-            };
-        Llamado(url,llevo,hacer);
-    });
-    
-    $("#DeleteDocentes").click(function(){
-             
-            var url= "RDM/Docentes/deletedocentes.html";
-            var llevo= "acceso=true";
-            var hacer=function(data){
-                $("#rdm").html(data);
-                
-            };
-        Llamado(url,llevo,hacer);
-    });
-    
-    $("#ChangeDocentes").click(function(){
-             
-            var url= "RDM/Docentes/changedocentes.html";
-            var llevo= "acceso=true";
-            var hacer=function(data){
-                $("#rdm").html(data);
-                
-            };
-        Llamado(url,llevo,hacer);
-    });
-    
-    $("#ListDocentes").click(function(){
-             
-            var url= "RDM/Docentes/listadocentes.html";
-            var llevo= "acceso=true";
-            var hacer=function(data){
-                $("#rdm").html(data);
-                
-            };
-        Llamado(url,llevo,hacer);
-    });
-     
-    }
-    function idDocente(){
-        var id = 0;
-        var limite = localstorage.length;
-        try{
-           for(var i=0;i<limite;i++){
-            var key= localstorage.key(i); 
-            var objeto= JSON.parse(localstorage.getItem(key));
-            if(obj.type=="doc"){
-               if(obj.id>id){
-                  id=obj;
-               } 
-            }
-        } 
-        }catch(e){
-            console.error("metodo idDocente presenta falla");
-        }
-        
-        return id;
-    }
-    function registarDocente (){
-        $("#regadmin").validate({
-           rules:{
-              nomdoc:{
-                  required:true,
-                  rangelenght:[3,20]
-              } ,
-              apedoc:{
-                  required:true,
-                  rangelenght:[3,20]
-              } ,
-              ccdoc:{
-                  required:true,
-                  rangelenght:[6,10]
-                  
-              } 
-           }, 
-           submitHandler:function(){
-               var iddo=idDocente();
-               var docente={
-                 id:iddo(),
-                 nom: $("#nomdoc").val(),  
-                 ape: $("#apedoc").val(),
-                 cc: $("#ccdoc").val(),
-                 type:"doc"
-               };
-               localstorage.setItem("doc",iddo,JSON.stringify(docente));
-               $("#nomdoc").val("");
-               $("#apedoc").val("");
-               $("#ccdoc").val("");
-               alert("El docente"+docente.nom+"fue almacenado");
-           }
-        });
-    }
-    
-    function listarDocente (){
-        $("#regdoc").validate({
-           rules:{
-              nomdoc:{
-                  required:true,
-                  rangelenght:[3,20]
-              } ,
-              apedoc:{
-                  required:true,
-                  rangelenght:[3,20]
-              } ,
-              ccdoc:{
-                  required:true,
-                  rangelenght:[6,10]
-              } 
-           }, 
-           submitHandler:function(){
-               var iddo=idDocente();
-               var docente={
-                 id:iddo(),
-                 nom:$("#nomdoc").val(),  
-                 ape:$("#apedoc").val(),
-                 cc:$("#ccdoc").val(),
-                 type:"doc"
-               };
-               localstorage.getItem("doc",iddo,JSON.stringify(docente));
-               $("#nomdoc").val("");
-               $("#apedoc").val("");
-               $("#ccdoc").val("");
-               alert("El docente"+docente.nom+"fue almacenado");
-           }
-        });
-    }
     
 });
  
--- JS/LogicaEstudiantes.js
+++ JS/LogicaEstudiantes.js
@@ -1,144 +1,225 @@
- 
- $(document).ready(function(){
-     
-     function Llamado(donde,quellevo,hacer){   
+$ (document).ready(function(){
+
+    function Llamado(donde, quellevo, hacer){
         $.ajax({
             url: donde,
             data: quellevo,
             type: 'GET',
-            dataType: 'html',            
+            dataType: 'html',
             success: function (data) {
-              hacer(data);  
-            },            
+                hacer(data);
+            },
             error: function (xhr, status) {
                 alert('Disculpe, existió un problema');
-            },
-            
-        });
-    } 
-    
-    $("#Estudiantes").click(function(){        
-            var url= "RDM/Administrador/opciones2.html";
-            var llevo= "acceso=true";
-            var hacer=function(data){
-                $("#rdm").html(data);
-                Estudiantes();
-            };
-        Llamado(url,llevo,hacer);
-    });
-    
-    function Estudiantes(){
-        $("#AddEstudiantes").click(function(){
-             
-            var url= "RDM/Administrador/addestudiante.html";
-            var llevo= "acceso=true";
-            var hacer=function(data){
-                $("#rdm").html(data);
-                
-            };
-        Llamado(url,llevo,hacer);
-    });
-    
-    
-    $("#ListEstudiantes").click(function(){
-             
-            var url= "RDM/Administrador/listestudiantes.html";
-            var llevo= "acceso=true";
-            var hacer=function(data){
-                $("#rdm").html(data);
-                
-            };
-        Llamado(url,llevo,hacer);
-    });
-     
-    }
-    function idEstudiante(){
-        var id=o;
-        var limite2=localstorage.length;
-        try{
-           for(var i=0;i<limite2;i++){
-            var key= localstorage.key(i); 
-            var objeto= JSON.parse(localstorage.setItem(key));
-            if(obj.type=="est"){
-               if(obj.id>id){
-                  id=obj;
-               } 
             }
-        } 
-        }catch(e){
-            console.error("metodo idEstudiante presenta falla");
+        });
+    }
+
+    //Menú administrador
+    //----Crear estudiante
+    $(document).on("click", "#crear_est", function () {
+        let url = "RDM/Estudiantes/addestudiante.html";
+        let llevo = "acceso=true";
+        let hacer = function(data){
+            $("#subContenido").html(data);
+            administrador("crear estudiante");
+
+        };
+        Llamado(url,llevo,hacer);
+    });
+    //---Listar estudiantes
+    $(document).on("click", ".estudiantes", function () {
+        let url = "RDM/Estudiantes/listaEstudiantes.html";
+        let llevo = "acceso=true";
+        let hacer = function(data){
+            $("#rdm").html(data);
+            administrador("listar estudiantes");
+        };
+        Llamado(url,llevo,hacer);
+    });
+    //-----Menú adminstrador
+
+    //Operaciones
+    function administrador(operacion, estudiante) {
+
+        switch (operacion) {
+            case "crear estudiante":
+                registrarEstudiante(false);
+                $("#titulo_form").html("Formulario de Registro");
+                $("#btn_estudiante").html("GUARDAR");
+                break;
+            case "editar estudiante":
+                registrarEstudiante(true, estudiante);
+                $("#titulo_form").html("Formulario de actualización");
+                $("#btn_estudiante").html("ACTUALIZAR");
+                break;
+            case "listar estudiantes":
+                listarEstudiantes();
+                break;
+            default:
+                eliminarEstudiante(estudiante);
+                break;
         }
-        
+    }
+
+    //Operaciones Estudiante
+    function registrarEstudiante(editar, estudianteExistente) {
+
+        let idEst = "";
+        let nombre = $("#nomest");
+        let apellido = $("#apeest");
+        let cedula = $("#ccest");
+        let mensaje = "";
+
+        if(editar){
+
+            idEst = estudianteExistente.id;
+            nombre.val(estudianteExistente.nom);
+            apellido.val(estudianteExistente.ape);
+            cedula.val(estudianteExistente.cc);
+            mensaje = "fue editado";
+
+        }else{
+            idEst = generarId();
+            mensaje = "fue almacenado";
+        }
+
+        $("#reg_est").validate({
+            rules:{
+                nomest:{
+                    required: true,
+                    rangelength: [3, 20]
+                },
+                apeest:{
+                    required:true,
+                    rangelength: [3, 20]
+                },
+                ccest:{
+                    required: true,
+                    rangelength: [6, 12],
+                    number: true
+                }
+            },
+            submitHandler:function(){
+
+                let estudiante = {
+                    id: idEst,
+                    nom: nombre.val(),
+                    ape: apellido.val(),
+                    cc: cedula.val(),
+                    type: "est"
+                };
+                localStorage.setItem("est" + idEst, JSON.stringify(estudiante));
+                nombre.val("");
+                apellido.val("");
+                cedula.val("");
+                alert("El estudiante "+ " "+ estudiante.nom +" "+ mensaje);
+                Llamado("RDM/Estudiantes/listaEstudiantes.html","acceso=true",function (data) {
+                    $("#rdm").html(data);
+                    administrador("listar estudiantes");
+                });
+            }
+        });
+    }
+
+    function buscarEstudiante(id) { //Busca un estudiante dentro del localStorage
+        let estudiante = {};
+        let key = "";
+
+        for(let i = 0; i < localStorage.length; i++){
+            key = localStorage.key(i);
+            let obj = JSON.parse(localStorage.getItem(key));
+            if(key === "est"+id){ //Se debe buscar por la key
+                estudiante = obj;
+            }
+        }
+        return {estudiante, key};
+    }
+
+    function listarEstudiantes() {
+        let datos_tabla = "";
+        let estudiantes = [];
+        //Guardar todos los estudiantes en un arreglo
+        for(let i = 0; i < localStorage.length; i++){
+            let key = localStorage.key(i);
+            let obj = JSON.parse(localStorage.getItem(key));
+            if(obj.type === "est"){
+                estudiantes.push(obj);//Guarda los objetos de tipo estudiante
+            }
+        }
+
+        //Pintar los estudiantes en la tabla
+        for(let j = 0; j < estudiantes.length; j++){
+            datos_tabla += `
+                <tr idEst="${estudiantes[j].id /*En cada fila se guarda el id del estudiante*/}">
+                    <td>${estudiantes[j].nom}</td>
+                    <td>${estudiantes[j].ape}</td>
+                    <td>${estudiantes[j].cc}</td>
+                    <td>
+                        <button class="btn btn-success editar">Editar</button>
+                    </td>
+                    <td>
+                        <button class="btn btn-danger eliminar">Eliminar</button>
+                    </td>
+                </tr>
+             `;
+        }
+
+        $("#lista_est").html(datos_tabla);
+
+        /*Como los botones son generados en el html por JS no se pude usar el atributo id sino una clase,
+        * ademas es necesario que cada evento sea asignado al mismo tiempo de su creacion si no es asi
+        * no se ejecutaria el evento click
+        * */
+
+        $(".editar").click(function () {
+            //Obtener el id del estudiante que se quiere editar
+            let id = $(this).parent().parent().attr("idEst");//dos veces parent() por que el id se encuentra en la etiqueta <tr>
+            Llamado("RDM/Estudiantes/addestudiante.html","acceso=true",function (data) {
+                $("#subContenido").html(data);
+                administrador("editar estudiante", buscarEstudiante(id).estudiante);
+            });
+        });
+
+        $(".eliminar").click(function () {
+            //Obtener el id del estudiante que se quiere editar
+            let id = $(this).parent().parent().attr("idEst");//dos veces parent() por que el id se encuentra en la etiqueta <tr>
+            if(confirm("¿Desea eliminar el estudiantes?")){
+                Llamado("RDM/Estudiantes/listaEstudiantes.html","acceso=true",function (data) {
+                    $("#rdm").html(data);
+                    administrador("borrar estudiante", buscarEstudiante(id).key);
+                });
+            }
+        });
+
+    }
+
+    function eliminarEstudiante(key) {
+        localStorage.removeItem(key);
+        listarEstudiantes();
+    }
+
+
+    function generarId(){ //Sirve para generar id del estudiante
+        let id = 0;
+        let limite = localStorage.length;
+        try{
+            for(let i = 0 ; i < limite; i++){
+                let key = localStorage.key(i);
+                let obj = JSON.parse(localStorage.getItem(key));
+                if(obj.type === "est"){
+                    if(obj.id === obj.id){
+                        id++;
+                    }
+                }
+            }
+        }catch(e){
+            console.error("metodo generarId presenta falla");
+        }
+
         return id;
     }
-    function registarEstudiante (){
-        $("#regdoc").validate({
-           rules:{
-              nomest:{
-                  required:true,
-                  rangelenght:[3,20]
-              } ,
-              apeest:{
-                  required:true,
-                  rangelenght:[3,20]
-              } ,
-              ccest:{
-                  required:true,
-                  rangelenght:[6,10]
-              } 
-           }, 
-           submitHandler:function(){
-               var iddo=idDocente();
-               var estudiante={
-                 id:iddo(),
-                 nom:$("#nomest").val(),  
-                 ape:$("#apeest").val(),
-                 cc:$("#ccest").val(),
-                 type:"est"
-               };
-               localstorage.setItem("est",iddo,JSON.stringify(estudiante));
-               $("#nomest").val("");
-               $("#apeest").val("");
-               $("#ccest").val("");
-               alert("El estudiante"+estudiante.nom+"fue almacenado");
-           }
-        });
-    }
-    
-    function listarEstudiante (){
-        $("#regdoc").validate({
-           rules:{
-              nomest:{
-                  required:true,
-                  rangelenght:[3,20]
-              } ,
-              apeest:{
-                  required:true,
-                  rangelenght:[3,20]
-              } ,
-              ccest:{
-                  required:true,
-                  rangelenght:[6,10]
-              } 
-           }, 
-           submitHandler:function(){
-               var iddo=idEstudiante();
-               var estudiante={
-                 id:iddo(),
-                 nom:$("#nomest").val(),  
-                 ape:$("#apeest").val(),
-                 cc:$("#ccest").val(),
-                 type:"doc"
-               };
-               localstorage.getItem("doc",iddo,JSON.stringify(estudiante));
-               $("#nomest").val("");
-               $("#apeest").val("");
-               $("#ccdest").val("");
-               alert("El estudiante"+estudiante.nom+"fue almacenado");
-           }
-        });
-    }
-    
+
+
+
 });
- 
- 
